4662ef5343f167df3ffce147e31e0967573bfc2a
Raul <rchfe23@gmail.com>
2018-09-19 16:19:09 -0400

    remove directories

--- data_analysis/analyzers/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-try:
-    from utility import genutil as util
-except ImportError:
-    sys.path.insert(0, os.path.expanduser("~/PycharmProjects/job-insights"))
-    from utility import genutil as util
--- scripts/generate_insights/run_analysis.py
+++ scripts/generate_insights/run_analysis.py
@@ -4,8 +4,7 @@ try:
     import job_analyzer as ja
 except ImportError:
     sys.path.insert(0, os.path.expanduser("~/PycharmProjects/job-insights/dev_jobs_insights"))
-    from data_analysis import job_analyzer as ja
-
+    from data_analysis.sandbox import job_analyzer as ja
 
 if __name__ == '__main__':
     data_analyzer = ja.JobDataAnalyzer(settings_path="config.ini")
