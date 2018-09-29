19ac5224537d4a74ad63c5f6a7c2fdbf1df6ebca
Raul <rchfe23@gmail.com>
2018-09-28 21:11:38 -0400

    update SQL schema and db script

--- database/create_db.py
+++ database/create_db.py
@@ -4,17 +4,17 @@ import sqlite3
 import time
 
 
-DB_FILENAME = os.path.expanduser("~/databases/job_data.sqlite")
+DB_FILENAME = os.path.expanduser("~/databases/rss_feeds.sqlite")
 SCHEMA_FILENAME = "job_data_schema.sql"
 
 
 if __name__ == '__main__':
     # Setup argument parser
     parser = argparse.ArgumentParser()
-    parser = argparse.ArgumentParser(description="Create SQLite database {}".format(DB_FILENAME))
+    parser = argparse.ArgumentParser(description="Create SQLite database '{}'".format(DB_FILENAME))
     parser.add_argument("-o", action="store_true", dest="overwrite",
                         default=False,
-                        help="Overwrite the db file {}".format(*DB_FILENAME))
+                        help="Overwrite the db file '{}'".format(DB_FILENAME))
     # Process command-line arguments
     results = parser.parse_args()
 
@@ -27,13 +27,15 @@ if __name__ == '__main__':
         os.remove(DB_FILENAME)
 
     if db_is_new or results.overwrite:
-        print("Creating db")
+        print("Creating database ...")
         with sqlite3.connect(DB_FILENAME) as conn:
             try:
                 with open(SCHEMA_FILENAME, 'rt') as f:
                     schema = f.read()
                     conn.executescript(schema)
             except IOError as e:
-                print("I/O error({}): {}".format(e.errno, e.strerror))
+                raise IOError(e)
+            else:
+                print("Database created!")
     else:
-        print("Database exists: ", DB_FILENAME)
+        print("Database '{}' already exists!".format(DB_FILENAME))
